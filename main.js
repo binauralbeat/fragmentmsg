7524a3e4f60bc83063c5e8b054d3bcfd6cb7b2e1
binauralbeat <binauralbeat001@gmail.com>
2018-04-16 14:51:20 -0500

    some fun styling

--- main.js
+++ main.js
@@ -4,14 +4,23 @@ const fragment = document.createDocumentFragment()
 
 const message1 = document.createElement("section")
 message1.textContent = "ducks are considered water fowl"
+const messageClass1 = document.createAttribute('class')
+messageClass1.value = 'right'
+message1.setAttributeNode(messageClass1)
 fragment.appendChild(message1)
 
 const message2 = document.createElement("section")
 message2.textContent = "I like bats"
+const messageClass = document.createAttribute('class')
+messageClass.value = 'left'
+message2.setAttributeNode(messageClass)
 fragment.appendChild(message2)
 
 const message3 = document.createElement("section")
 message3.textContent = "you my friend are rude, bats are mammals"
+const messageClass3 = document.createAttribute('class')
+messageClass3.value = 'right'
+message3.setAttributeNode(messageClass3)
 fragment.appendChild(message3)
 
 const message4 = document.createElement("section")
